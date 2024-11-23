bde2883df9e4d62984b48fbcf66ce8fa6ee3aac6
joao bassi <filpi.joao@gmail.com>
2024-11-23 15:51:59 -0300

    filpijoao 23/11 16h00

---
+++ class.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+
+class SoybeanPrediciton:
+
+    def __init__(self, image, model):
+        self.image = image
+        self.model = model
+
+    def predict(self):
+
+       img_array = img_to_array(self.image)
+       x = np.expand_dims(img_array, axis=0)
+       pred = self.model.predict(x)
+       numero_classe = list(list(pred)[0]).index(pred.max())
+       classe_lista = ['Broken soybeans', 'Immature soybeans', 'Intact soybeans',
+                       'Skin-damaged soybeans', 'Spotted soybeans']
+       return classe_lista[numero_classe]
